b236cdf94af8c40f46a92a27b995cd2c086687d1
Mohammad Hasan Pouhasani <mr.se.mhp@gmail.com>
2022-04-08 19:32:35 +0430

    add customize styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                darkBlueMd: '#243B55',
+                darkBlueLg: '#141E30',
+            },
+            translate: {
+                84: '21rem',
+                88: '22rem',
+                120: '30rem',
+            },
+            borderWidth: {
+                1: '1px',
+            },
+        },
     },
     plugins: [],
 };
