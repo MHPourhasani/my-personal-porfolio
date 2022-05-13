2d31b13212a3296caf27690e0f74311361b5465d
Mohammad Hasan Pouhasani <mr.se.mhp@gmail.com>
2022-05-14 00:50:20 +0430

    target social media link to blank page

--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -8,168 +8,147 @@ import { BsWhatsapp } from 'react-icons/bs';
 import { MdKeyboardArrowRight } from 'react-icons/md';
 
 const ContactMe = () => {
-    const form = useRef();
-    const [error, serError] = useState(null);
+	const form = useRef();
+	const [error, serError] = useState(null);
 
-    const sendEmail = (e) => {
-        e.preventDefault();
+	const sendEmail = (e) => {
+		e.preventDefault();
 
-        emailjs
-            .sendForm(
-                'service_5qqvqvm',
-                'template_2u87qwf',
-                form.current,
-                'p4hqvptU_QHTZdlGj',
-            )
-            .then(
-                (result) => {
-                    console.log(result.text);
-                    toast.success('Email is sent', {
-                        position: 'top-right',
-                        autoClose: 3000,
-                        hideProgressBar: false,
-                        closeOnClick: true,
-                        pauseOnHover: true,
-                        draggable: true,
-                        progress: undefined,
-                        theme: 'dark',
-                    });
-                },
-                (error) => {
-                    if (error.text) {
-                        serError(error.text);
-                    }
-                    console.log(error);
-                },
-            );
-            toast.success('Email is sent', {
-                position: 'top-right',
-                autoClose: 3000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: 'dark',
-            });
-    };
+		emailjs
+			.sendForm('service_5qqvqvm', 'template_2u87qwf', form.current, 'p4hqvptU_QHTZdlGj')
+			.then(
+				(result) => {
+					console.log(result.text);
+					toast.success('Email is sent', {
+						position: 'top-right',
+						autoClose: 3000,
+						hideProgressBar: false,
+						closeOnClick: true,
+						pauseOnHover: true,
+						draggable: true,
+						progress: undefined,
+						theme: 'dark',
+					});
+				},
+				(error) => {
+					if (error.text) {
+						serError(error.text);
+					}
+					console.log(error);
+				}
+			);
+		toast.success('Email is sent', {
+			position: 'top-right',
+			autoClose: 3000,
+			hideProgressBar: false,
+			closeOnClick: true,
+			pauseOnHover: true,
+			draggable: true,
+			progress: undefined,
+			theme: 'dark',
+		});
+	};
 
-    return (
-        <section
-            id="contact-me"
-            className="py-5 flex flex-col flex-wrap justify-center items-center md:py-14 md:mx-5"
-        >
-            <section className="flex flex-col justify-center items-center my-5">
-                <p className="text-xs text-gray-500">Get in Touch</p>
-                <p className="font-semibold text-lg text-sky-400 my-1">
-                    Contact Me
-                </p>
-            </section>
+	return (
+		<section
+			id='contact-me'
+			className='flex flex-col flex-wrap items-center justify-center py-5 md:mx-5 md:py-14'>
+			<section className='my-5 flex flex-col items-center justify-center'>
+				<p className='text-xs text-gray-500'>Get in Touch</p>
+				<p className='my-1 text-lg font-semibold text-sky-400'>Contact Me</p>
+			</section>
 
-            <section className="w-full flex flex-col justify-center items-start sm:flex-row">
-                <ul className="w-full flex flex-col justify-center items-center gap-y-3">
-                    <li className="w-5/6 py-5 bg-darkBlueMd text-white flex flex-col justify-center items-center border-2 border-transparent rounded-md hover:bg-opacity-40 hover:border-darkBlueMd transition-all delay-75 sm:w-7/12 lg:w-1/2">
-                        <HiOutlineMail className="w-6 h-6 mb-2" />
-                        <p className="font-semibold my-1">Email</p>
-                        <p className="text-sm my-0.5">
-                            mhpourhasani.dev@gmail.com
-                        </p>
-                        <a
-                            href="www.linkedin.com/in/mohammad-hasan-pourhasani"
-                            className="text-sky-400 flex justify-center items-center"
-                        >
-                            <p className="mr-0.5 text-xs">
-                                Send a message
-                            </p>
-                            <MdKeyboardArrowRight className="mt-1 text-sm" />
-                        </a>
-                    </li>
+			<section className='flex w-full flex-col items-start justify-center sm:flex-row'>
+				<ul className='flex w-full flex-col items-center justify-center gap-y-3'>
+					<li className='flex w-5/6 flex-col items-center justify-center rounded-md border-2 border-transparent bg-darkBlueMd py-5 text-white transition-all delay-75 hover:border-darkBlueMd hover:bg-opacity-40 sm:w-7/12 lg:w-1/2'>
+						<HiOutlineMail className='mb-1 h-6 w-6' />
+						<p className='my-1 font-semibold'>Email</p>
+						<p className='my-0.5 text-sm'>mhpourhasani.dev@gmail.com</p>
+						<a
+							target='_blank'
+							rel='noreferrer'
+							href='www.linkedin.com/in/mohammad-hasan-pourhasani'
+							className='mt-1 flex items-center justify-center text-sky-400'>
+							<p className='mr-0.5 text-xs'>Send a message</p>
+							<MdKeyboardArrowRight className='mt-1 text-sm' />
+						</a>
+					</li>
 
-                    <li className="w-5/6 py-5 bg-darkBlueMd text-white flex flex-col justify-center items-center border-2 border-transparent rounded-md hover:bg-opacity-40 hover:border-darkBlueMd transition-all delay-75 sm:w-7/12 lg:w-1/2">
-                        <SiLinkedin className="w-6 h-6 mb-2" />
-                        <p className="my-1">Linkedin</p>
-                        <p className="text-sm my-0.5">
-                            in/mohammad-hasan-pourhasani
-                        </p>
+					<li className='flex w-5/6 flex-col items-center justify-center rounded-md border-2 border-transparent bg-darkBlueMd py-5 text-white transition-all delay-75 hover:border-darkBlueMd hover:bg-opacity-40 sm:w-7/12 lg:w-1/2'>
+						<SiLinkedin className='mb-1 h-6 w-6' />
+						<p className='my-1'>Linkedin</p>
+						<p className='my-0.5 text-sm'>in/mohammad-hasan-pourhasani</p>
 
-                        <a
-                            href="www.linkedin.com/in/mohammad-hasan-pourhasani"
-                            className="text-sky-400 flex justify-center items-center"
-                        >
-                            <p className="mr-0.5 text-xs">
-                                Send a message
-                            </p>
-                            <MdKeyboardArrowRight className="mt-1 text-sm" />
-                        </a>
-                    </li>
+						<a
+							target='_blank'
+							rel='noreferrer'
+							href='www.linkedin.com/in/mohammad-hasan-pourhasani'
+							className='mt-1 flex items-center justify-center text-sky-400'>
+							<p className='mr-0.5 text-xs'>Send a message</p>
+							<MdKeyboardArrowRight className='mt-1 text-sm' />
+						</a>
+					</li>
 
-                    <li className="w-5/6 py-5 bg-darkBlueMd text-white flex flex-col justify-center items-center border-2 border-transparent rounded-md hover:bg-opacity-40 hover:border-darkBlueMd transition-all delay-75 sm:w-7/12 lg:w-1/2">
-                        <BsWhatsapp className="w-6 h-6 mb-2" />
-                        <p className="my-1">WhatsApp</p>
-                        <p className="text-sm my-0.5">
-                            +98 9109231061
-                        </p>
-                        <a
-                            href="https://api.whatsapp.com/send?phone=+989109231061"
-                            className="text-sky-400 flex justify-center items-center"
-                        >
-                            <p className="mr-0.5 text-xs">
-                                Send a message
-                            </p>
-                            <MdKeyboardArrowRight className="mt-1 text-sm" />
-                        </a>
-                    </li>
-                </ul>
+					<li className='flex w-5/6 flex-col items-center justify-center rounded-md border-2 border-transparent bg-darkBlueMd py-5 text-white transition-all delay-75 hover:border-darkBlueMd hover:bg-opacity-40 sm:w-7/12 lg:w-1/2'>
+						<BsWhatsapp className='mb-1 h-6 w-6' />
+						<p className='my-1'>WhatsApp</p>
+						<p className='my-0.5 text-sm'>+98 9109231061</p>
+						<a
+							target='_blank'
+							rel='noreferrer'
+							href='https://api.whatsapp.com/send?phone=+989109231061'
+							className='mt-1 flex items-center justify-center text-sky-400'>
+							<p className='mr-0.5 text-xs'>Send a message</p>
+							<MdKeyboardArrowRight className='mt-1 text-sm' />
+						</a>
+					</li>
+				</ul>
 
-                <form
-                    ref={form}
-                    onSubmit={sendEmail}
-                    className="w-full text-white flex flex-col justify-center items-center sm:items-start"
-                >
-                    <div className="w-5/6 my-2 flex flex-col justify-center items-start sm:w-full">
-                        <input
-                            type="text"
-                            name="name"
-                            placeholder="Your Name"
-                            required
-                            className="w-full bg-transparent text-xs border-2 border-gray-700 rounded-md outline-none px-2 py-3 sm:w-10/12 lg:w-8/12"
-                        />
-                    </div>
+				<form
+					ref={form}
+					onSubmit={sendEmail}
+					className='flex w-full flex-col items-center justify-center text-white sm:items-start'>
+					<div className='my-2 flex w-5/6 flex-col items-start justify-center sm:w-full'>
+						<input
+							type='text'
+							name='name'
+							placeholder='Your Name'
+							required
+							className='w-full rounded-md border-2 border-gray-700 bg-transparent px-2 py-3 text-xs outline-none sm:w-10/12 lg:w-8/12'
+						/>
+					</div>
 
-                    <div className="w-5/6 my-2 flex flex-col justify-center items-start sm:w-full">
-                        <input
-                            type="email"
-                            name="user_email"
-                            required
-                            placeholder="Your Email"
-                            className="w-full bg-transparent text-xs border-2 border-gray-700 rounded-md outline-none px-2 py-3 sm:w-10/12 lg:w-8/12"
-                        />
-                    </div>
+					<div className='my-2 flex w-5/6 flex-col items-start justify-center sm:w-full'>
+						<input
+							type='email'
+							name='user_email'
+							required
+							placeholder='Your Email'
+							className='w-full rounded-md border-2 border-gray-700 bg-transparent px-2 py-3 text-xs outline-none sm:w-10/12 lg:w-8/12'
+						/>
+					</div>
 
-                    <div className="w-5/6 my-2 flex flex-col justify-center items-start sm:w-full">
-                        <input
-                            type="text"
-                            name="message"
-                            placeholder="Your Message"
-                            required
-                            className="w-full bg-transparent text-xs border-2 border-gray-700 rounded-md outline-none px-2 py-3 sm:w-10/12 lg:w-8/12"
-                        />
-                    </div>
+					<div className='my-2 flex w-5/6 flex-col items-start justify-center sm:w-full'>
+						<input
+							type='text'
+							name='message'
+							placeholder='Your Message'
+							required
+							className='w-full rounded-md border-2 border-gray-700 bg-transparent px-2 py-3 text-xs outline-none sm:w-10/12 lg:w-8/12'
+						/>
+					</div>
 
-                    <button
-                        type="submit"
-                        className="bg-sky-400 text-xs p-3 rounded-md my-1"
-                    >
-                        Send Message
-                    </button>
+					<button type='submit' className='my-1 rounded-md bg-sky-400 p-3 text-xs'>
+						Send Message
+					</button>
 
-                    {error && <p className="text-red-600">{error}</p>}
-                </form>
+					{error && <p className='text-red-600'>{error}</p>}
+				</form>
 
-                {/* <ContactMeComp /> */}
-            </section>
-        </section>
-    );
+				{/* <ContactMeComp /> */}
+			</section>
+		</section>
+	);
 };
 
 export default ContactMe;
