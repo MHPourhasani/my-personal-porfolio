eb135c366d624e944af77e9f883274cb1cbec986
Mohammad Hasan Pouhasani <mr.se.mhp@gmail.com>
2022-05-13 07:30:58 +0430

    create my services component

---
+++ src/components/MyServices/MyServices.jsx
@@ -0,0 +1,17 @@
+const MyServices = () => {
+    return (
+        <section
+            id="my-services"
+            className="py-5 flex flex-col flex-wrap justify-center items-center md:py-14 md:mx-5"
+        >
+            <section className="flex flex-col justify-center items-center my-5">
+                <p className="text-xs text-gray-500">Get in Touch</p>
+                <p className="font-semibold text-lg text-sky-400 my-1">
+                    My Services
+                </p>
+            </section>
+        </section>
+    );
+};
+
+export default MyServices;
