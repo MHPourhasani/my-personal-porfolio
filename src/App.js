9479676aa17ab83dc12e4382db7441bf901e25c8
Mohammad Hasan Pouhasani <mr.se.mhp@gmail.com>
2022-05-13 07:36:56 +0430

    import components to render and add react-toastify

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import AboutMe from './components/AboutMe/AboutMe';
+import ContactMe from './components/ContactMe/ContactMe';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import MyProjects from './components/MyProjects/MyProjects';
+import MyServices from './components/MyServices/MyServices';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  return (
-    <div className="">
-      
-    </div>
-  );
+    return (
+        <div id="Home" className="bg-darkBlueLg">
+            <ToastContainer />
+
+            <Header />
+            <AboutMe />
+            <MyProjects />
+            <MyServices />
+            <ContactMe />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
