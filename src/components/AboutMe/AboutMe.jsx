103c5212a614dbabc7c05972d82068cb19860df9
Mohammad Hasan Pouhasani <mr.se.mhp@gmail.com>
2022-05-14 01:34:05 +0430

    add exprience content junior

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -5,82 +5,64 @@ import { HiUserGroup } from 'react-icons/hi';
 import { VscFolderLibrary } from 'react-icons/vsc';
 
 const AboutMe = () => {
-    return (
-        <section
-            id="about-me"
-            className="py-5 flex flex-col flex-wrap justify-center items-center md:py-14 md:mx-5"
-        >
-            <section className="flex flex-col justify-center items-center">
-                <p className="text-xs text-gray-500">Get To Know</p>
-                <p className="font-semibold text-lg text-sky-400 my-1">
-                    About Me
-                </p>
-            </section>
+	return (
+		<section
+			id='about-me'
+			className='flex w-full flex-col flex-wrap items-center justify-around py-5 md:py-14'>
+			<section className='flex flex-col items-center justify-center'>
+				<p className='text-xs text-gray-500'>Get To Know</p>
+				<p className='my-1 text-lg font-semibold text-sky-400'>About Me</p>
+			</section>
 
-            <section className="flex flex-col flex-wrap justify-center items-center md:flex-row md:my-5">
-                <div className="hidden md:block md:w-64 md:h-64 bg-sky-400 rounded-lg hover:opacity-90">
-                    <span className="bg-sky-400 rounded-lg md:w-64 md:h-64 hover:rotate-12"></span>
-                    <img
-                        src={photoMeSqu}
-                        alt="my-photo-Square"
-                        className="-translate-y-32"
-                    />
-                </div>
+			<section className='flex flex-col flex-wrap items-center justify-center md:my-5 md:flex-row'>
+				<div className='hidden rounded-lg bg-sky-400 hover:opacity-90 md:block md:h-64 md:w-64'>
+					<img
+						src={photoMeSqu}
+						aria-hidden
+						alt='my-photo-Square'
+						className='-translate-y-32'
+					/>
+				</div>
 
-                <div className="w-full md:w-1/2 lg:ml-12">
-                    <ul className="flex flex-col justify-center items-center sm:flex-row sm:flex-wrap">
-                        <li className="w-2/3 py-5 my-2 flex flex-col justify-center items-center bg-darkBlueMd rounded-md sm:w-1/4 sm:m-2 md:px-5">
-                            <GiRibbonMedal className="text-sky-400 w-7 h-7 mb-2" />
-                            <p className="font-semibold text-white my-1">
-                                Experience
-                            </p>
-                            <p className="text-gray-400 text-sm">
-                                Experience
-                            </p>
-                        </li>
+				<div className='w-full md:w-1/2 lg:ml-16'>
+					<ul className='flex flex-col items-center justify-center sm:flex-row sm:flex-wrap'>
+						<li className='my-2 flex w-2/3 flex-col items-center justify-center rounded-md bg-darkBlueMd py-5 sm:mr-2 sm:w-1/4 md:px-5'>
+							<GiRibbonMedal className='mb-2 h-7 w-7 text-sky-400' />
+							<p className='my-1 font-semibold text-white'>Experience</p>
+							<p className='text-sm text-gray-400'>Junior</p>
+						</li>
 
-                        <li className="w-2/3 py-5 my-2 flex flex-col justify-center items-center bg-darkBlueMd rounded-md sm:w-1/4 sm:m-2 md:px-5">
-                            <HiUserGroup className="text-sky-400 w-7 h-7 mb-2" />
-                            <p className="font-semibold text-white my-1">
-                                Clients
-                            </p>
-                            <p className="text-gray-400 text-sm">
-                                Experience
-                            </p>
-                        </li>
+						<li className='my-2 flex w-2/3 flex-col items-center justify-center rounded-md bg-darkBlueMd py-5 sm:m-2 sm:w-1/4 md:px-5'>
+							<HiUserGroup className='mb-2 h-7 w-7 text-sky-400' />
+							<p className='my-1 font-semibold text-white'>Clients</p>
+							<p className='text-sm text-gray-400'>Experience</p>
+						</li>
 
-                        <li className="w-2/3 py-5 my-2 flex flex-col justify-center items-center bg-darkBlueMd rounded-md sm:w-1/4 sm:m-2 md:px-5">
-                            <VscFolderLibrary className="text-sky-400 w-7 h-7 mb-2" />
+						<li className='my-2 flex w-2/3 flex-col items-center justify-center rounded-md bg-darkBlueMd py-5 sm:ml-2 sm:w-1/4 md:px-5'>
+							<VscFolderLibrary className='mb-2 h-7 w-7 text-sky-400' />
 
-                            <p className="font-semibold text-white my-1">
-                                Projects
-                            </p>
-                            <p className="text-gray-400 text-sm">
-                                Experience
-                            </p>
-                        </li>
-                    </ul>
+							<p className='my-1 font-semibold text-white'>Projects</p>
+							<p className='text-sm text-gray-400'>Experience</p>
+						</li>
+					</ul>
 
-                    <section className="w-2/3 mx-auto text-sm sm:w-11/12 sm:px-10 md:w-10/12 md:px-0">
-                        <p className="mx-auto text-gray-400">
-                            Lorem ipsum dolor sit, amet consectetur
-                            adipisicing elit. Omnis facere fugit
-                            atque, ipsum architecto nam quo quas odit
-                            fuga reprehenderit quaerat quisquam natus
-                            ipsam impedit repellat deleniti quae
-                            dolorem culpa!
-                        </p>
-                        <a
-                            href="#contact-me"
-                            className="bg-sky-400 px-3 py-2 m-5 rounded-md"
-                        >
-                            Let's Talk
-                        </a>
-                    </section>
-                </div>
-            </section>
-        </section>
-    );
+					<section className='mx-auto my-2 w-2/3 text-sm sm:w-11/12 sm:px-10 md:my-0 md:w-10/12 md:px-2.5'>
+						<p className='mb-5 text-gray-400'>
+							Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis facere
+							fugit atque, ipsum architecto nam quo quas odit fuga reprehenderit
+							quaerat quisquam natus ipsam impedit repellat deleniti quae dolorem
+							culpa!
+						</p>
+						<a
+							href='#contact-me'
+							className='my-5 rounded-md bg-sky-400 px-3 py-2 font-medium'>
+							Let's Talk
+						</a>
+					</section>
+				</div>
+			</section>
+		</section>
+	);
 };
 
 export default AboutMe;
