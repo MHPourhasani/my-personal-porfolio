05a4185f87b68f94e89195954057e5c969247e70
Mohammad Hasan Pouhasani <mr.se.mhp@gmail.com>
2022-05-13 07:30:22 +0430

    create my project component and add projects

---
+++ src/components/MyProjects/MyProjects.jsx
@@ -0,0 +1,228 @@
+import { Link } from 'react-router-dom';
+import { AiOutlineGithub } from 'react-icons/ai';
+import { GiEarthAmerica } from 'react-icons/gi';
+
+const MyProjects = () => {
+    return (
+        <section
+            id="my-projects"
+            className="py-5 flex flex-col flex-wrap justify-center items-center md:py-14 md:mx-5"
+        >
+            <section className="flex flex-col justify-center items-center">
+                <p className="text-xs text-gray-500">fghj</p>
+                <p className="font-semibold text-lg text-sky-400 my-1">
+                    My Projects
+                </p>
+            </section>
+
+            <ul className="flex flex-wrap justify-center items-start">
+                <li className="w-5/6 bg-darkBlueMd px-3 my-2 border-2 border-transparent rounded-md flex flex-col justify-center items-start hover:bg-opacity-40 hover:border-darkBlueMd transition-all delay-75 sm:w-64 sm:m-4">
+                    <div>
+                        <img />
+                    </div>
+                    <div className="font-semibold text-base text-white">
+                        Shopping Carts
+                    </div>
+
+                    <ul className="flex justify-start items-center my-2 font-normal text-xs text-gray-500">
+                        <li className="border-1 rounded-sm px-1 py-0.5 mr-1">
+                            Html
+                        </li>
+                        <li className="border-1 rounded-sm px-1 py-0.5 mx-1">
+                            Css
+                        </li>
+                        <li className="border-1 rounded-sm px-1 py-0.5 mx-1">
+                            Tailwindcss
+                        </li>
+                        <li className="border-1 rounded-sm px-1 py-0.5 mx-1">
+                            Js
+                        </li>
+                        <li className="border-1 rounded-sm px-1 py-0.5 mx-1">
+                            React
+                        </li>
+                    </ul>
+
+                    <div className="text-gray-500 text-sm pb-5">
+                        Lorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Fugiat, maxime facere id
+                        quidem illo, cupiditate repellat perspiciatis
+                        nihil adipisci inventore architecto excepturi
+                        voluptate. Dignissimos perspiciatis error
+                        deserunt fugit, sapiente dolorum!
+                    </div>
+                    <div className="flex justify-center items-center mb-5">
+                        <a
+                            href="https://github.com/MHPourhasani/shopping-cards"
+                            className="w-28 h-9 flex justify-center items-center py-1 mr-1 bg-sky-500 text-center rounded-md"
+                        >
+                            <AiOutlineGithub className="mr-1 w-6 h-6" />
+                            <p>github</p>
+                        </a>
+                        <a
+                            href=""
+                            className="w-28 h-9 flex justify-center items-center py-1 ml-1 text-gray-500 bg-darkBlueLg rounded-md"
+                        >
+                            <GiEarthAmerica className="mr-1 w-6 h-6" />
+                            <p>demo</p>
+                        </a>
+                    </div>
+                </li>
+
+                <li className="w-5/6 bg-darkBlueMd px-3 my-2 border-2 border-transparent rounded-md flex flex-col justify-center items-start hover:bg-opacity-40 hover:border-darkBlueMd transition-all delay-75 sm:w-64 sm:m-4">
+                    <div>
+                        <img />
+                    </div>
+                    <div className="font-semibold text-base text-white">
+                        Contact App
+                    </div>
+
+                    <ul className="flex justify-start items-center my-2 font-normal text-xs text-gray-500">
+                        <li className="border-1 rounded-sm px-1 py-0.5 mr-1">
+                            Html
+                        </li>
+                        <li className="border-1 rounded-sm px-1 py-0.5 mx-1">
+                            Css
+                        </li>
+                        <li className="border-1 rounded-sm px-1 py-0.5 mx-1">
+                            Tailwindcss
+                        </li>
+                        <li className="border-1 rounded-sm px-1 py-0.5 mx-1">
+                            Js
+                        </li>
+                        <li className="border-1 rounded-sm px-1 py-0.5 mx-1">
+                            React
+                        </li>
+                    </ul>
+
+                    <div className="text-gray-500 text-sm pb-5">
+                        Lorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Fugiat, maxime facere id
+                        quidem illo, cupiditate repellat perspiciatis
+                        nihil adipisci inventore architecto excepturi
+                        voluptate. Dignissimos perspiciatis error
+                        deserunt fugit, sapiente dolorum!
+                    </div>
+                    <div className="flex justify-center items-center mb-5">
+                        <a
+                            href="https://github.com/MHPourhasani/Contact-App"
+                            className="w-28 h-9 flex justify-center items-center py-1 mr-1 bg-sky-500 text-center rounded-md"
+                        >
+                            <AiOutlineGithub className="mr-1 w-6 h-6" />
+                            <p>github</p>
+                        </a>
+                        <a
+                            href=""
+                            className="w-28 h-9 flex justify-center items-center py-1 ml-1 text-gray-500 bg-darkBlueLg rounded-md"
+                        >
+                            <GiEarthAmerica className="mr-1 w-6 h-6" />
+                            <p>demo</p>
+                        </a>
+                    </div>
+                </li>
+
+                <li className="w-5/6 bg-darkBlueMd px-3 my-2 border-2 border-transparent rounded-md flex flex-col justify-center items-start hover:bg-opacity-40 hover:border-darkBlueMd transition-all delay-75 sm:w-64 sm:m-4">
+                    <div>
+                        <img />
+                    </div>
+                    <div className="font-semibold text-base text-white">
+                        Expence Tracker
+                    </div>
+
+                    <ul className="flex justify-start items-center my-2 font-normal text-xs text-gray-500">
+                        <li className="border-1 rounded-sm px-1 py-0.5 mr-1">
+                            Html
+                        </li>
+                        <li className="border-1 rounded-sm px-1 py-0.5 mx-1">
+                            Css
+                        </li>
+                        <li className="border-1 rounded-sm px-1 py-0.5 mx-1">
+                            Tailwindcss
+                        </li>
+                        <li className="border-1 rounded-sm px-1 py-0.5 mx-1">
+                            Js
+                        </li>
+                        <li className="border-1 rounded-sm px-1 py-0.5 mx-1">
+                            React
+                        </li>
+                    </ul>
+
+                    <div className="text-gray-500 text-sm pb-5">
+                        Lorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Fugiat, maxime facere id
+                        quidem illo, cupiditate repellat perspiciatis
+                        nihil adipisci inventore architecto excepturi
+                        voluptate. Dignissimos perspiciatis error
+                        deserunt fugit, sapiente dolorum!
+                    </div>
+                    <div className="flex justify-center items-center mb-5">
+                        <a
+                            href=""
+                            className="w-28 h-9 flex justify-center items-center py-1 mr-1 bg-sky-500 text-center rounded-md"
+                        >
+                            <AiOutlineGithub className="mr-1 w-6 h-6" />
+                            <p>github</p>
+                        </a>
+                        <a
+                            href=""
+                            className="w-28 h-9 flex justify-center items-center py-1 ml-1 text-gray-500 bg-darkBlueLg rounded-md"
+                        >
+                            <GiEarthAmerica className="mr-1 w-6 h-6" />
+                            <p>demo</p>
+                        </a>
+                    </div>
+                </li>
+
+                <li className="w-5/6 bg-darkBlueMd px-3 my-2 border-2 border-transparent rounded-md flex flex-col justify-center items-start hover:bg-opacity-40 hover:border-darkBlueMd transition-all delay-75 sm:w-64 sm:m-4">
+                    <div>
+                        <img />
+                    </div>
+                    <div className="font-semibold text-base text-white">
+                        Snapp
+                    </div>
+
+                    <ul className="flex justify-start items-center my-2 font-normal text-xs text-gray-500">
+                        <li className="border-1 rounded-sm px-1 py-0.5 mr-1">
+                            Html
+                        </li>
+                        <li className="border-1 rounded-sm px-1 py-0.5 mx-1">
+                            Css
+                        </li>
+                        <li className="border-1 rounded-sm px-1 py-0.5 mx-1">
+                            Tailwindcss
+                        </li>
+                        <li className="border-1 rounded-sm px-1 py-0.5 mx-1">
+                            Js
+                        </li>
+                    </ul>
+
+                    <div className="text-gray-500 text-sm pb-5">
+                        Lorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Fugiat, maxime facere id
+                        quidem illo, cupiditate repellat perspiciatis
+                        nihil adipisci inventore architecto excepturi
+                        voluptate. Dignissimos perspiciatis error
+                        deserunt fugit, sapiente dolorum!
+                    </div>
+                    <div className="flex justify-center items-center mb-5">
+                        <a
+                            href=""
+                            className="w-28 h-9 flex justify-center items-center py-1 mr-1 bg-sky-500 text-center rounded-md"
+                        >
+                            <AiOutlineGithub className="mr-1 w-6 h-6" />
+                            <p>github</p>
+                        </a>
+                        <a
+                            href=""
+                            className="w-28 h-9 flex justify-center items-center py-1 ml-1 text-gray-500 bg-darkBlueLg rounded-md"
+                        >
+                            <GiEarthAmerica className="mr-1 w-6 h-6" />
+                            <p>demo</p>
+                        </a>
+                    </div>
+                </li>
+            </ul>
+        </section>
+    );
+};
+
+export default MyProjects;
