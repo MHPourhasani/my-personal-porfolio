54d2e09fccae68b34a35a049a6623303a8bb9e0a
Mohammad Hasan Pouhasani <mhpourhasani.dev@gmail.com>
2022-08-25 02:09:19 +0430

    improvement some styles

--- src/components/Header/Navigation.jsx
+++ src/components/Header/Navigation.jsx
@@ -1,78 +1,78 @@
-import { NavLink } from 'react-router-dom';
-import { BiPhoneCall, BiMenuAltLeft } from 'react-icons/bi';
-import { AiOutlineHome } from 'react-icons/ai';
+import { useState } from 'react';
+
+// icons
+import { TbSmartHome } from 'react-icons/tb';
 import { FiUsers } from 'react-icons/fi';
 import { VscProject } from 'react-icons/vsc';
 import { TiContacts } from 'react-icons/ti';
 import { RiServiceLine } from 'react-icons/ri';
-import { useState } from 'react';
 
 const Navigation = () => {
-    const [activeNav, setActiveNav] = useState('#');
+	const [activeNav, setActiveNav] = useState('#introduction');
 
-    return (
-        <nav className="bg-slate-900 flex justify-center items-center fixed bottom-5 p-2 rounded-full">
-            <a
-                href="#"
-                onClick={() => setActiveNav('#')}
-                className={
-                    activeNav === '#'
-                        ? 'p-1 mx-2 text-white bg-slate-800 rounded-full md:mx-3'
-                        : 'text-gray-400 p-1 mx-2 hover:bg-slate-800 hover:text-white hover:rounded-full md:mx-3'
-                }
-            >
-                <AiOutlineHome className="w-5 h-5" />
-            </a>
+	return (
+		<nav className='fixed bottom-5 z-10 flex items-center justify-center rounded-full bg-slate-900 p-2 shadow-md shadow-slate-800'>
+			{/* home icon */}
+			<a
+				href='#introduction'
+				onClick={() => setActiveNav('#introduction')}
+				className={
+					activeNav === '#introduction'
+						? 'mx-2 rounded-full bg-slate-800 p-1 text-white md:mx-3'
+						: 'mx-2 p-1 text-gray-400 hover:rounded-full hover:bg-slate-800 hover:text-white md:mx-3'
+				}>
+				<TbSmartHome className='h-7 w-7 xl:h-8 xl:w-8' />
+			</a>
 
-            <a
-                href="#about-me"
-                onClick={() => setActiveNav('#about-me')}
-                className={
-                    activeNav === '#about-me'
-                        ? 'p-1 mx-2 text-white bg-slate-800 rounded-full md:mx-3'
-                        : 'text-gray-400 p-1 mx-2 hover:bg-slate-800 hover:text-white hover:rounded-full md:mx-3'
-                }
-            >
-                <FiUsers className="w-5 h-5" />
-            </a>
+			{/* about me icon */}
+			<a
+				href='#about-me'
+				onClick={() => setActiveNav('#about-me')}
+				className={
+					activeNav === '#about-me'
+						? 'mx-2 rounded-full bg-slate-800 p-1.5 text-white md:mx-3'
+						: 'mx-2 p-1.5 text-gray-400 hover:rounded-full hover:bg-slate-800 hover:text-white md:mx-3'
+				}>
+				<FiUsers className='h-6 w-6 xl:h-7 xl:w-7' />
+			</a>
 
-            <a
-                href="#my-projects"
-                onClick={() => setActiveNav('#my-projects')}
-                className={
-                    activeNav === '#my-projects'
-                        ? 'p-1 mx-2 text-white bg-slate-800 rounded-full md:mx-3'
-                        : 'text-gray-400 p-1 mx-2 hover:bg-slate-800 hover:text-white hover:rounded-full md:mx-3'
-                }
-            >
-                <VscProject className="w-5 h-5" />
-            </a>
+			{/* projects icon */}
+			<a
+				href='#my-projects'
+				onClick={() => setActiveNav('#my-projects')}
+				className={
+					activeNav === '#my-projects'
+						? 'mx-2 rounded-full bg-slate-800 p-1.5 text-white md:mx-3'
+						: 'mx-2 p-1.5 text-gray-400 hover:rounded-full hover:bg-slate-800 hover:text-white md:mx-3'
+				}>
+				<VscProject className='h-6 w-6 xl:h-7 xl:w-7' />
+			</a>
 
-            <a
-                href="#my-services"
-                onClick={() => setActiveNav('#my-services')}
-                className={
-                    activeNav === '#my-services'
-                        ? 'p-1 mx-2 text-white bg-slate-800 rounded-full md:mx-3'
-                        : 'text-gray-400 p-1 mx-2 hover:bg-slate-800 hover:text-white hover:rounded-full md:mx-3'
-                }
-            >
-                <RiServiceLine className="w-5 h-5" />
-            </a>
+			{/* services icon */}
+			<a
+				href='#my-services'
+				onClick={() => setActiveNav('#my-services')}
+				className={
+					activeNav === '#my-services'
+						? 'mx-2 rounded-full bg-slate-800 p-1.5 text-white md:mx-3'
+						: 'mx-2 p-1.5 text-gray-400 hover:rounded-full hover:bg-slate-800 hover:text-white md:mx-3'
+				}>
+				<RiServiceLine className='h-6 w-6 xl:h-7 xl:w-7' />
+			</a>
 
-            <a
-                href="#contact-me"
-                onClick={() => setActiveNav('#contact-me')}
-                className={
-                    activeNav === '#contact-me'
-                        ? 'p-1 mx-2 text-white bg-slate-800 rounded-full md:mx-3'
-                        : 'text-gray-400 p-1 mx-2 hover:bg-slate-800 hover:text-white hover:rounded-full md:mx-3'
-                }
-            >
-                <TiContacts className="w-6 h-6" />
-            </a>
-        </nav>
-    );
+			{/* contact me icon */}
+			<a
+				href='#contact-me'
+				onClick={() => setActiveNav('#contact-me')}
+				className={
+					activeNav === '#contact-me'
+						? 'mx-2 rounded-full bg-slate-800 p-1 text-white md:mx-3'
+						: 'mx-2 p-1 text-gray-400 hover:rounded-full hover:bg-slate-800 hover:text-white md:mx-3'
+				}>
+				<TiContacts className='h-7 w-7 xl:h-8 xl:w-8' />
+			</a>
+		</nav>
+	);
 };
 
 export default Navigation;
