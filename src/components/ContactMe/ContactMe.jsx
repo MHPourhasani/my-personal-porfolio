b0c88f20accf7e3ee115ec2c2531e2fd07c09f96
Mohammad Hasan Pouhasani <mhpourhasani.dev@gmail.com>
2022-10-07 01:36:14 +0330

    improved styles in ContactMe component for better display on different screens

--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -3,7 +3,7 @@ import emailjs from '@emailjs/browser';
 import { toast } from 'react-toastify';
 
 // icons
-import { SiLinkedin } from 'react-icons/si';
+import { FaLinkedinIn } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
 import { BsWhatsapp } from 'react-icons/bs';
 import { MdKeyboardArrowRight } from 'react-icons/md';
@@ -20,8 +20,7 @@ const ContactMe = () => {
 			.sendForm('service_5qqvqvm', 'template_2u87qwf', form.current, 'p4hqvptU_QHTZdlGj')
 			.then(
 				(result) => {
-					console.log(result.text);
-					toast.success('Email sent', {
+					toast.success('Email sent successfully', {
 						position: 'top-right',
 						autoClose: 3000,
 						hideProgressBar: false,
@@ -36,7 +35,6 @@ const ContactMe = () => {
 					if (error.text) {
 						serError(error.text);
 					}
-					console.log(error);
 					toast.error('The email was not sent', {
 						position: 'top-right',
 						autoClose: 3000,
@@ -54,57 +52,57 @@ const ContactMe = () => {
 	return (
 		<section
 			id='contact-me'
-			className='flex flex-col flex-wrap items-center justify-center py-5 md:mx-5 md:py-14'>
-			<section className='my-5 flex flex-col items-center justify-center'>
+			className='flex w-full flex-col items-center justify-center py-5 md:py-14'>
+			<section className='my-5 flex flex-col items-center justify-center lg:mb-10'>
 				<p className='text-xs text-gray-500'>Get in Touch</p>
-				<p className='my-1 text-lg font-semibold text-sky-400'>Contact Me</p>
+				<p className='my-1.5 text-lg font-semibold text-sky-400 md:text-xl'>Contact Me</p>
 			</section>
 
-			<section className='flex w-full flex-col items-start justify-start sm:flex-row'>
+			<section className='flex w-full flex-col items-start justify-center sm:w-11/12 sm:flex-row sm:justify-between sm:gap-5 md:w-10/12 md:gap-10 lg:w-8/12 lg:gap-16'>
 				{/* Reach me on social media section */}
-				<ul className='flex w-full flex-col items-center justify-center gap-y-3'>
+				<ul className='flex w-full flex-col items-center justify-center gap-3 sm:w-9/12 lg:w-9/12'>
 					{/* my email section */}
-					<li className='flex w-5/6 flex-col items-center justify-center rounded-md border-2 border-transparent bg-darkBlueMd py-5 text-white transition-all delay-75 hover:border-darkBlueMd hover:bg-opacity-40 sm:w-7/12 lg:w-1/2'>
-						<HiOutlineMail className='mb-1 h-6 w-6' />
-						<p className='my-1 font-semibold'>Email</p>
-						<p className='my-0.5 text-sm'>mhpourhasani.dev@gmail.com</p>
+					<li className='flex w-5/6 flex-col items-center justify-center gap-2 rounded-md bg-darkBlueMd py-5 text-white ease-in-out hover:scale-105 sm:w-full'>
+						<HiOutlineMail className='h-auto w-8 lg:w-9' />
+						<p className='text-lg font-semibold lg:text-xl'>Email</p>
+						<p className='text-sm text-gray-300'>mhpourhasani.dev@gmail.com</p>
 						<a
 							target='_blank'
 							rel='noreferrer'
 							href='www.linkedin.com/in/mohammad-hasan-pourhasani'
-							className='mt-1 flex items-center justify-center text-sky-400'>
-							<p className='mr-0.5 text-xs'>Send a message</p>
+							className='mt-1 flex items-center justify-center text-sky-400 hover:animate-bounce hover:text-sky-500'>
+							<p className='mr-0.5 text-sm lg:text-sm'>Send a message</p>
 							<MdKeyboardArrowRight className='mt-1 text-sm' />
 						</a>
 					</li>
 
 					{/* my linkedin section */}
-					<li className='flex w-5/6 flex-col items-center justify-center rounded-md border-2 border-transparent bg-darkBlueMd py-5 text-white transition-all delay-75 hover:border-darkBlueMd hover:bg-opacity-40 sm:w-7/12 lg:w-1/2'>
-						<SiLinkedin className='mb-1 h-6 w-6' />
-						<p className='my-1'>Linkedin</p>
-						<p className='my-0.5 text-sm'>in/mohammad-hasan-pourhasani</p>
+					<li className='flex w-5/6 flex-col items-center justify-center gap-2 rounded-md bg-darkBlueMd py-5 text-white ease-in-out hover:scale-105 sm:w-full'>
+						<FaLinkedinIn className='h-auto w-7 lg:w-8' />
+						<p className='text-lg font-semibold lg:text-xl'>Linkedin</p>
+						<p className='text-sm text-gray-300'>in/mohammad-hasan-pourhasani</p>
 
 						<a
 							target='_blank'
 							rel='noreferrer'
 							href='www.linkedin.com/in/mohammad-hasan-pourhasani'
-							className='mt-1 flex items-center justify-center text-sky-400'>
-							<p className='mr-0.5 text-xs'>Send a message</p>
+							className='mt-1 flex items-center justify-center text-sky-400 hover:animate-bounce hover:text-sky-500'>
+							<p className='mr-0.5 text-sm lg:text-sm'>Send a message</p>
 							<MdKeyboardArrowRight className='mt-1 text-sm' />
 						</a>
 					</li>
 
 					{/* my whatsApp section */}
-					<li className='flex w-5/6 flex-col items-center justify-center rounded-md border-2 border-transparent bg-darkBlueMd py-5 text-white transition-all delay-75 hover:border-darkBlueMd hover:bg-opacity-40 sm:w-7/12 lg:w-1/2'>
-						<BsWhatsapp className='mb-1 h-6 w-6' />
-						<p className='my-1'>WhatsApp</p>
-						<p className='my-0.5 text-sm'>+98 9109231061</p>
+					<li className='flex w-5/6 flex-col items-center justify-center gap-2 rounded-md bg-darkBlueMd py-5 text-white ease-in-out hover:scale-105 sm:w-full'>
+						<BsWhatsapp className='h-auto w-7 lg:w-8' />
+						<p className='text-lg font-semibold lg:text-xl'>WhatsApp</p>
+						<p className='text-sm text-gray-300'>+98 9109231061</p>
 						<a
 							target='_blank'
 							rel='noreferrer'
 							href='https://api.whatsapp.com/send?phone=+989109231061'
-							className='mt-1 flex items-center justify-center text-sky-400'>
-							<p className='mr-0.5 text-xs'>Send a message</p>
+							className='mt-1 flex items-center justify-center text-sky-400 hover:animate-bounce hover:text-sky-500'>
+							<p className='mr-0.5 text-sm lg:text-sm'>Send a message</p>
 							<MdKeyboardArrowRight className='mt-1 text-sm' />
 						</a>
 					</li>
@@ -114,42 +112,38 @@ const ContactMe = () => {
 				<form
 					ref={form}
 					onSubmit={sendEmail}
-					className='flex w-full flex-col items-center justify-center text-white sm:items-start'>
+					className='mt-3 flex w-full flex-col items-center justify-center gap-3 text-sm text-white sm:mt-0 '>
 					{/* user name input */}
-					<div className='my-2 flex w-5/6 flex-col items-start justify-center sm:w-full'>
-						<input
-							type='text'
-							name='user_name'
-							placeholder='Your Name'
-							required
-							className='w-full rounded-md border-2 border-gray-700 bg-transparent px-2 py-3 text-xs outline-none sm:w-10/12 lg:w-8/12'
-						/>
-					</div>
+					<input
+						type='text'
+						name='user_name'
+						placeholder='Your Name'
+						required
+						className='w-5/6 rounded-md border-2 border-gray-700 bg-transparent p-2.5 outline-none focus:border-white sm:w-full md:p-3.5'
+					/>
 
 					{/* user email input */}
-					<div className='my-2 flex w-5/6 flex-col items-start justify-center sm:w-full'>
-						<input
-							type='email'
-							name='user_email'
-							required
-							placeholder='Your Email'
-							className='w-full rounded-md border-2 border-gray-700 bg-transparent px-2 py-3 text-xs outline-none sm:w-10/12 lg:w-8/12'
-						/>
-					</div>
+					<input
+						type='email'
+						name='user_email'
+						required
+						placeholder='Your Email'
+						className='w-5/6 rounded-md border-2 border-gray-700 bg-transparent p-2.5 outline-none focus:border-white sm:w-full md:p-3.5'
+					/>
 
 					{/* user message input */}
-					<div className='my-2 flex w-5/6 flex-col items-start justify-start sm:w-full'>
-						<input
-							type='text'
-							name='message'
-							placeholder='Your Message...'
-							required
-							className='h-32 w-full rounded-md border-2 border-gray-700 bg-transparent px-2 py-3 text-xs outline-none sm:w-10/12 lg:w-8/12'
-						/>
-					</div>
+					<textarea
+						type='text'
+						name='message'
+						placeholder='Your Message...'
+						required
+						className='h-32 max-h-56 w-5/6 rounded-md border-2 border-gray-700 bg-transparent p-2.5 text-start outline-none focus:border-white sm:w-full md:p-3.5'
+					/>
 
 					{/* send message btn */}
-					<button type='submit' className='my-1 rounded-md bg-sky-400 p-3 text-xs'>
+					<button
+						type='submit'
+						className='w-5/6 rounded-md bg-sky-500 p-3 hover:bg-sky-600 sm:w-full md:p-3.5'>
 						Send Message
 					</button>
 
