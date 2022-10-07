230ff8eb504fff493b17108084147647f339ffc9
Mohammad Hasan Pouhasani <mhpourhasani.dev@gmail.com>
2022-10-07 13:29:29 +0330

    create projectsData.js to save projects information and map on it in MyProjects comp

--- src/components/MyProjects/MyProjects.jsx
+++ src/components/MyProjects/MyProjects.jsx
@@ -1,233 +1,122 @@
-import { AiOutlineGithub, AiFillHtml5 } from 'react-icons/ai';
+// icons
+import { AiOutlineGithub } from 'react-icons/ai';
 import { GiEarthAmerica } from 'react-icons/gi';
-import { DiCss3 } from 'react-icons/di';
-import { SiTailwindcss, SiJavascript, SiReact } from 'react-icons/si';
 
-import snapp_demo_screenShot from '../../assets/img/snapp_demo_screenShot.png';
+// Import Swiper core and required modules and Import Swiper styles
+import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+// projects data
+import { projectsData } from '../../projectsData';
 
 const MyProjects = () => {
 	return (
 		<section
 			id='my-projects'
-			className='flex flex-col flex-wrap items-center justify-center py-5 md:mx-5 md:py-14'>
+			className='flex w-full flex-col items-center justify-center gap-3 py-5 md:py-14'>
 			<section className='flex flex-col items-center justify-center'>
-				<p className='text-xs text-gray-500'>fghj</p>
-				<p className='my-1 text-lg font-semibold text-sky-400'>My Projects</p>
+				<p className='text-xs text-gray-500 sm:text-sm'>Get To Know</p>
+				<p className='my-1.5 text-lg font-semibold text-sky-400 md:text-xl'>My Projects</p>
 			</section>
 
-			<ul className='flex flex-wrap items-start justify-center'>
-				<li className='my-2 flex w-5/6 flex-col items-start justify-center rounded-md border-2 border-transparent bg-darkBlueMd p-3 hover:border-darkBlueMd hover:bg-opacity-80 sm:m-4 sm:w-64'>
-					<div>
-						<img aria-hidden />
-					</div>
-					<div className='text-base font-semibold text-white'>Shopping Carts</div>
+			<Swiper
+				cssMode={true}
+				navigation={true}
+				mousewheel={true}
+				keyboard={true}
+				pagination={{
+					clickable: true,
+				}}
+				breakpoints={{
+					320: {
+						width: 320,
+						slidesPerView: 1,
+						centeredSlides: true,
+						spaceBetween: 10,
+					},
+					640: {
+						width: 640,
+						slidesPerView: 2,
+						centeredSlides: false,
+						spaceBetween: -40,
+					},
+					768: {
+						width: 768,
+						slidesPerView: 2,
+						spaceBetween: -10,
+					},
+					1024: {
+						width: 1024,
+						slidesPerView: 3,
+						spaceBetween: -80,
+					},
+					1280: {
+						width: 1280,
+						slidesPerView: 3,
+						spaceBetween: -100,
+					},
+				}}
+				modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+				className='items my-3 flex w-11/12 justify-center xl:w-11/12'>
+				{projectsData.map((project) => (
+					<SwiperSlide key={project.id}>
+						<li className='flex w-full flex-col items-start justify-center gap-3 rounded-md bg-darkBlueMd p-3 sm:w-10/12 md:w-11/12 lg:w-9/12'>
+							{/* project image */}
+							<a target='_blank' rel='noreferrer' href={project.demoLink}>
+								<img
+									src={project.image}
+									alt={`${project.name} project`}
+									aria-hidden
+									className='rounded-md hover:scale-105'
+								/>
+							</a>
 
-					<div className='py-1 text-sm text-gray-500'>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, maxime
-						facere id quidem illo, cupiditate repellat perspiciatis nihil adipisci
-						inventore architecto excepturi voluptate. Dignissimos perspiciatis error
-						deserunt fugit, sapiente dolorum!
-					</div>
+							{/* project name */}
+							<div className='text-base font-semibold text-white hover:text-darkBlueLg md:text-lg'>
+								<a target='_blank' rel='noreferrer' href={project.demoLink}>
+									{project.name}
+								</a>
+							</div>
 
-					<ul className='my-2 flex items-center justify-start text-xs font-normal text-gray-400'>
-						<li className='mr-1 rounded-md bg-slate-800 px-1.5 py-1'>
-							<AiFillHtml5 className='h-5 w-5 text-orange-600' />
+							{/* project description */}
+							<div className='text-sm text-gray-500 xl:text-base'>
+								{project.description}
+							</div>
+
+							{/* technologies used in project */}
+							<ul className='flex items-center gap-1.5'>
+								{project.technologiesUsed.map((technology) => (
+									<li className='rounded-md bg-slate-800 p-1.5'>{technology}</li>
+								))}
+							</ul>
+
+							{/* github and demo project link */}
+							<div className='flex w-full items-center justify-between gap-2'>
+								<a
+									target='_blank'
+									rel='noreferrer'
+									href={project.githubLink}
+									className='flex flex-1 items-center justify-center rounded-md bg-sky-500 py-2 hover:bg-sky-600'>
+									<AiOutlineGithub className='mr-1 h-6 w-6' />
+									<p>Github</p>
+								</a>
+								<a
+									target='_blank'
+									rel='noreferrer'
+									href={project.demoLink}
+									className='flex flex-1 items-center justify-center rounded-md bg-darkBlueLg py-2 text-gray-500 hover:bg-slate-800 hover:text-gray-200'>
+									<GiEarthAmerica className='mr-1 h-6 w-6' />
+									<p>Demo</p>
+								</a>
+							</div>
 						</li>
-
-						<li className='mr-1 rounded-md bg-slate-800 px-1.5 py-1'>
-							<DiCss3 className='h-5 w-5 text-blue-500' />
-						</li>
-
-						<li className='mr-1 rounded-md bg-slate-800 px-1.5 py-1'>
-							<SiTailwindcss className='h-5 w-5 text-sky-400' />
-						</li>
-
-						<li className='mr-1 rounded-md bg-slate-800 px-1.5 py-1'>
-							<SiReact className='h-5 w-5 text-sky-400' />
-						</li>
-					</ul>
-
-					<div className='my-1 flex items-center justify-center'>
-						<a
-							target='_blank'
-							rel='noreferrer'
-							href='https://github.com/MHPourhasani/shopping-cards'
-							className='mr-1 flex h-9 w-28 items-center justify-center rounded-md bg-sky-500 py-1 text-center'>
-							<AiOutlineGithub className='mr-1 h-6 w-6' />
-							<p>github</p>
-						</a>
-						<a
-							target='_blank'
-							rel='noreferrer'
-							href='#'
-							className='ml-1 flex h-9 w-28 items-center justify-center rounded-md bg-darkBlueLg py-1 text-gray-500 hover:text-gray-200'>
-							<GiEarthAmerica className='mr-1 h-6 w-6' />
-							<p>demo</p>
-						</a>
-					</div>
-				</li>
-
-				<li className='my-2 flex w-5/6 flex-col items-start justify-center rounded-md border-2 border-transparent bg-darkBlueMd p-3 hover:border-darkBlueMd hover:bg-opacity-80 sm:m-4 sm:w-64'>
-					<div>
-						<img aria-hidden />
-					</div>
-					<div className='text-base font-semibold text-white'>Contact App</div>
-
-					<div className='py-1 text-sm text-gray-500'>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, maxime
-						facere id quidem illo, cupiditate repellat perspiciatis nihil adipisci
-						inventore architecto excepturi voluptate. Dignissimos perspiciatis error
-						deserunt fugit, sapiente dolorum!
-					</div>
-
-					<ul className='my-2 flex items-center justify-start text-xs font-normal text-gray-400'>
-						<li className='mr-1 rounded-md bg-slate-800 px-1.5 py-1'>
-							<AiFillHtml5 className='h-5 w-5 text-orange-600' />
-						</li>
-
-						<li className='mr-1 rounded-md bg-slate-800 px-1.5 py-1'>
-							<DiCss3 className='h-5 w-5 text-blue-500' />
-						</li>
-
-						<li className='mr-1 rounded-md bg-slate-800 px-1.5 py-1'>
-							<SiTailwindcss className='h-5 w-5 text-sky-400' />
-						</li>
-
-						<li className='mr-1 rounded-md bg-slate-800 px-1.5 py-1'>
-							<SiReact className='h-5 w-5 text-sky-400' />
-						</li>
-					</ul>
-
-					<div className='my-1 flex items-center justify-center'>
-						<a
-							target='_blank'
-							rel='noreferrer'
-							href='https://github.com/MHPourhasani/Contact-App'
-							className='mr-1 flex h-9 w-28 items-center justify-center rounded-md bg-sky-500 py-1 text-center'>
-							<AiOutlineGithub className='mr-1 h-6 w-6' />
-							<p>github</p>
-						</a>
-						<a
-							target='_blank'
-							rel='noreferrer'
-							href=''
-							className='ml-1 flex h-9 w-28 items-center justify-center rounded-md bg-darkBlueLg py-1 text-gray-500 hover:text-gray-200'>
-							<GiEarthAmerica className='mr-1 h-6 w-6' />
-							<p>demo</p>
-						</a>
-					</div>
-				</li>
-
-				<li className='my-2 flex w-5/6 flex-col items-start justify-center rounded-md border-2 border-transparent bg-darkBlueMd p-3 hover:border-darkBlueMd hover:bg-opacity-80 sm:m-4 sm:w-64'>
-					<div>
-						<img />
-					</div>
-					<div className='text-base font-semibold text-white'>Expence Tracker</div>
-
-					<div className='py-1 text-sm text-gray-500'>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, maxime
-						facere id quidem illo, cupiditate repellat perspiciatis nihil adipisci
-						inventore architecto excepturi voluptate. Dignissimos perspiciatis error
-						deserunt fugit, sapiente dolorum!
-					</div>
-
-					<ul className='my-2 flex items-center justify-start text-xs font-normal text-gray-400'>
-						<li className='mr-1 rounded-md bg-slate-800 px-1.5 py-1'>
-							<AiFillHtml5 className='h-5 w-5 text-orange-600' />
-						</li>
-
-						<li className='mr-1 rounded-md bg-slate-800 px-1.5 py-1'>
-							<DiCss3 className='h-5 w-5 text-blue-500' />
-						</li>
-
-						<li className='mr-1 rounded-md bg-slate-800 px-1.5 py-1'>
-							<SiTailwindcss className='h-5 w-5 text-sky-400' />
-						</li>
-
-						<li className='mr-1 rounded-md bg-slate-800 px-1.5 py-1'>
-							<SiReact className='h-5 w-5 text-sky-400' />
-						</li>
-					</ul>
-
-					<div className='my-1 flex items-center justify-center'>
-						<a
-							target='_blank'
-							rel='noreferrer'
-							href=''
-							className='mr-1 flex h-9 w-28 items-center justify-center rounded-md bg-sky-500 py-1 text-center'>
-							<AiOutlineGithub className='mr-1 h-6 w-6' />
-							<p>github</p>
-						</a>
-						<a
-							target='_blank'
-							rel='noreferrer'
-							href=''
-							className='ml-1 flex h-9 w-28 items-center justify-center rounded-md bg-darkBlueLg py-1 text-gray-500 hover:text-gray-200'>
-							<GiEarthAmerica className='mr-1 h-6 w-6' />
-							<p>demo</p>
-						</a>
-					</div>
-				</li>
-
-				<li className='my-2 flex w-5/6 flex-col items-start justify-center rounded-md border-2 border-transparent bg-darkBlueMd p-3 hover:border-darkBlueMd hover:bg-opacity-80 sm:m-4 sm:w-64'>
-					<div>
-						<a
-							target='_blank'
-							rel='noreferrer'
-							href='https://snapp-demo-mhpourhasani.vercel.app/'>
-							<img
-								aria-hidden
-								src={snapp_demo_screenShot}
-								alt='snapp demo image'
-								className='rounded-md hover:opacity-75'
-							/>
-						</a>
-					</div>
-					<div className='mt-1 text-base font-semibold text-white'>
-						<a href='https://snapp-demo-mhpourhasani.vercel.app/'>Snapp</a>
-					</div>
-
-					<div className='py-1 text-sm text-gray-500'>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, maxime
-						facere id quidem illo, cupiditate repellat perspiciatis nihil adipisci
-						inventore architecto excepturi voluptate. Dignissimos perspiciatis error
-						deserunt fugit, sapiente dolorum!
-					</div>
-
-					<ul className='my-2 flex items-center justify-start text-xs font-normal text-gray-400'>
-						<li className='mr-1 rounded-md bg-slate-800 px-1.5 py-1'>
-							<AiFillHtml5 className='h-5 w-5 text-orange-600' />
-						</li>
-
-						<li className='mr-1 rounded-md bg-slate-800 px-1.5 py-1'>
-							<DiCss3 className='h-5 w-5 text-blue-500' />
-						</li>
-
-						<li className='mr-1 rounded-md bg-slate-800 px-1.5 py-1'>
-							<SiTailwindcss className='h-5 w-5 text-sky-400' />
-						</li>
-					</ul>
-
-					<div className='my-1 flex items-center justify-center'>
-						<a
-							target='_blank'
-							rel='noreferrer'
-							href='https://github.com/MHPourhasani/Snapp-demo'
-							className='mr-1 flex h-9 w-28 items-center justify-center rounded-md bg-sky-500 py-1 text-center'>
-							<AiOutlineGithub className='mr-1 h-6 w-6' />
-							<p>github</p>
-						</a>
-						<a
-							target='_blank'
-							rel='noreferrer'
-							href='https://snapp-demo-mhpourhasani.vercel.app/'
-							className='ml-1 flex h-9 w-28 items-center justify-center rounded-md bg-darkBlueLg py-1 text-gray-500 hover:text-gray-200'>
-							<GiEarthAmerica className='mr-1 h-6 w-6' />
-							<p>demo</p>
-						</a>
-					</div>
-				</li>
-			</ul>
+					</SwiperSlide>
+				))}
+			</Swiper>
 		</section>
 	);
 };
---
+++ src/projectsData.js
@@ -0,0 +1,87 @@
+// images
+import shopping_cart_screenShot from './assets/img/shopping_cart_screenShot.jpg';
+import expense_tracker_screenShot from './assets/img/expense-tracker_screenShot.png';
+import snapp_clone_screenShot from './assets/img/snapp_clone_screenShot.png';
+import basalam_clone_screenShot from './assets/img/basalam_clone_screenShot.jpg';
+
+// icons
+import { AiFillHtml5 } from 'react-icons/ai';
+import { DiCss3 } from 'react-icons/di';
+import { SiTailwindcss, SiJavascript, SiReact } from 'react-icons/si';
+import { FaSass } from 'react-icons/fa';
+
+export const projectsData = [
+	{
+		id: 1,
+		name: 'Shopping Carts',
+		demoLink: 'https://shopping-carts-mhpourhasani.vercel.app/',
+		githubLink: 'https://github.com/MHPourhasani/shopping-carts-react',
+		image: shopping_cart_screenShot,
+		description:
+			'o This app is a project which user can see some products, sign up, login and add products to cart also manage cart. Implement by HTML, CSS, Tailwindcss and React.js. In this project I used json API for data and mongo db test on localhost. I working with Axios library for post and get data from the json data also i used react router dom and context. I used Formik library to form validation. This project has been implemented in a responsive design.',
+		technologiesUsed: [
+			<AiFillHtml5 className='h-5 w-5 text-orange-600' />,
+			<DiCss3 className='h-5 w-5 text-blue-500' />,
+			<SiTailwindcss className='h-5 w-5 text-sky-400' />,
+			<SiReact className='h-5 w-5 text-sky-400' />,
+		],
+	},
+	{
+		id: 3,
+		name: 'Expence Tracker',
+		demoLink: 'https://shopping-carts-mhpourhasani.vercel.app/',
+		githubLink: 'https://github.com/MHPourhasani/expence-tracker-react',
+		image: expense_tracker_screenShot,
+		description:
+			'o This app is a project which allows you to monitor and categorize your expenses. Implement by HTML, CSS and React.js . This project has been implemented in a responsive design. You can see live in this link and also see source code on Github.',
+		technologiesUsed: [
+			<AiFillHtml5 className='h-5 w-5 text-orange-600' />,
+			<DiCss3 className='h-5 w-5 text-blue-500' />,
+			<SiTailwindcss className='h-5 w-5 text-sky-400' />,
+			<SiReact className='h-5 w-5 text-sky-400' />,
+		],
+	},
+	{
+		id: 6,
+		name: 'Todo List js',
+		demoLink: 'https://todo-list-js-mhpourhasani.vercel.app/',
+		githubLink: 'https://github.com/MHPourhasani/todo-list-js',
+		image: null,
+		description:
+			'o	Implement by HTML, CSS and Javascript. This project has been implemented in a responsive design. You can see live in this link and also see source code on Github.',
+		technologiesUsed: [
+			<AiFillHtml5 className='h-5 w-5 text-orange-600' />,
+			<DiCss3 className='h-5 w-5 text-blue-500' />,
+			<SiTailwindcss className='h-5 w-5 text-sky-400' />,
+			<SiJavascript className='h-5 w-5 text-yellow-400' />,
+		],
+	},
+	{
+		id: 7,
+		name: 'Basalam Clone',
+		demoLink: 'https://basalam-clone-mhpourhasani.vercel.app/',
+		githubLink: 'https://github.com/MHPourhasani/basalam-clone',
+		image: basalam_clone_screenShot,
+		description:
+			'o	Implement by HTML, Sass and Javascript. This project has been implemented in a responsive design. You can see live in this link and also see source code on Github.',
+		technologiesUsed: [
+			<AiFillHtml5 className='h-5 w-5 text-orange-600' />,
+			<DiCss3 className='h-5 w-5 text-blue-500' />,
+			<FaSass className='h-5 w-5 text-pink-500' />,
+		],
+	},
+	{
+		id: 8,
+		name: 'Snapp Clone',
+		demoLink: 'http://snapp-clone-mhpourhasani.vercel.app/',
+		githubLink: 'https://github.com/MHPourhasani/snapp-clone',
+		image: snapp_clone_screenShot,
+		description:
+			'o	Implement by HTML, Css and Javascript. This project has been implemented in a responsive design. You can see live in this link and also see source code on Github.',
+		technologiesUsed: [
+			<AiFillHtml5 className='h-5 w-5 text-orange-600' />,
+			<DiCss3 className='h-5 w-5 text-blue-500' />,
+			<SiTailwindcss className='h-5 w-5 text-sky-400' />,
+		],
+	},
+];
