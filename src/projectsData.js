5fa33bd95fea84a96be26f4be5550cbf7865d502
MHPourhasani <mhpourhasani.dev@gmail.com>
2024-02-23 22:15:51 +0330

    update projects

--- src/projectsData.js
+++ src/projectsData.js
@@ -43,20 +43,6 @@ export const projectsData = [
 		],
 	},
 	{
-		name: 'Shopping Carts',
-		demoLink: 'https://shopping-carts-mhpourhasani.vercel.app/',
-		githubLink: 'https://github.com/MHPourhasani/shopping-carts-react',
-		image: shopping_cart_screenShot,
-		description:
-			'o This app is a project which user can see some products, sign up, login and add products to cart also manage cart. Implement by HTML, CSS, Tailwindcss and React.js. In this project I used json API for data and mongo db test on localhost. I working with Axios library for post and get data from the json data also i used react router dom and context. I used Formik library to form validation. This project has been implemented in a responsive design.',
-		technologiesUsed: [
-			<AiFillHtml5 className='h-5 w-5 text-orange-600' />,
-			<DiCss3 className='h-5 w-5 text-blue-500' />,
-			<SiTailwindcss className='h-5 w-5 text-sky-400' />,
-			<SiReact className='h-5 w-5 text-sky-400' />,
-		],
-	},
-	{
 		name: 'Expense Tracker',
 		demoLink: 'https://expense-tracker-react-mhpourhasani.vercel.app/',
 		githubLink: 'https://github.com/MHPourhasani/expense-tracker-react',
